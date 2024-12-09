b83a5f498dde0562ec2ea5cb2cba3f7d90254603
dhanushka <dhanushkadilshancc@gmail.com>
2024-12-09 20:18:43 +0530

    create dto and repo

---
+++ src/main/java/com/abc/userx/repository/UsersRepo.java
@@ -0,0 +1,12 @@
+package com.abc.userx.repository;
+
+
+import com.abc.userx.entity.OurUsers;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UsersRepo extends JpaRepository<OurUsers, Integer> {
+
+    Optional<OurUsers> findByEmail(String email);
+}
